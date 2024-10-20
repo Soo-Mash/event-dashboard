aec964d5070b4704fb9f40661d56cecc7a0a853d
Simon Jones <simon@thedelta.io>
2024-10-20 21:19:53 +0100

    Parent rendering page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,31 @@
+'use client';
+import EventCardList from '@/components/EventCardList/EventCardList';
+import { Container, Divider, Snackbar, SnackbarCloseReason } from '@mui/material';
+import { useState } from 'react';
+
 const HomePage = () => {
-  return <>Home</>;
+   const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
+
+   const handleOpenSnackbar = () => {
+      setSnackbarOpen(true);
+   };
+
+   const handleCloseSnackbar = (_, reason?: SnackbarCloseReason) => {
+      if (reason === 'clickaway') return;
+      setSnackbarOpen(false);
+   };
+   return (
+      <Container maxWidth="xl" sx={{ paddingLeft: '10px', paddingRight: '10px' }}>
+         <h2>Welcome back, Simon</h2>
+         <Divider sx={{ marginBottom: 3 }} />
+         <EventCardList hanndleOpenSnackbar={() => handleOpenSnackbar()} />
+         <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={4000}
+            onClose={handleCloseSnackbar}
+            message="This is a dummy feature that has no functionality"
+         />
+      </Container>
+   );
 };
 export default HomePage;
